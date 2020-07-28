eb3bbe9deb1fda749dd553df16e9c1349b9b0d1d
Vini Moraes <57679672+viniguidotti@users.noreply.github.com>
2020-07-28 01:42:38 -0300

    add

--- test.js
+++ test.js
@@ -3,13 +3,9 @@ const crypto = require("crypto");
 const start = Date.now();
 
 function logHashTime() {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+    crypto.pbkdf2("a", "b", 1000000, 512, "sha512", () => {
         console.log(`Hash: ${Date.now() - start}`);
     })
-
-    logHashTime();
-    logHashTime();
-    logHashTime();
-    logHashTime();
-
 }
+
+logHashTime();
